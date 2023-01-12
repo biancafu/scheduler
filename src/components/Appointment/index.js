da4fe13f4bb9ec70ff361dedf7934c07c4fef599
Bianca Fu <0509biancafu@gmail.com>
2023-01-12 15:13:33 -0700

    updated so that appointments are showing on the schedule page

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import "components/Appointment/styles.scss";
+import Header from './Header';
 import Show from "./Show"
+import Empty from './Empty';
+
 
 
 const Appointment = function (props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show interviewer={props.interview.interviewer} student={props.interview.student}/> : <Empty />}
+    </article>
   );
 }
 

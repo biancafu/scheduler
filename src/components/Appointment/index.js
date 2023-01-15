d5849bfffa361f687c1843a89aa7dd58ba8f0ad0
Bianca Fu <0509biancafu@gmail.com>
2023-01-15 13:01:51 -0700

    edited appoitment/index to use useVisualMode to generate view according to mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,14 +3,31 @@ import "components/Appointment/styles.scss";
 import Header from './Header';
 import Show from "./Show"
 import Empty from './Empty';
-
+import Form from './Form';
+import useVisualMode from "../hooks/useVisualMode";
 
 
 const Appointment = function (props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show interviewer={props.interview.interviewer} student={props.interview.student}/> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && <Form
+       interviewers={[]}
+       onSave={()=>console.log("hi")}
+       onCancel={back}
+       />}
     </article>
   );
 }

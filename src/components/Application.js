2f166a2eec08192a2afd987acc4c0656004b00e0
Bianca Fu <0509biancafu@gmail.com>
2023-01-16 18:00:14 -0700

    completed front end and back end for adding new apointments. set up bookInterview function to do so, and put data using axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "./Appointment/index";
-import { getAppointmentsForDay, getInterview } from "./helpers/selectors";
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "./helpers/selectors";
 
 export default function Application(props) {
 
@@ -14,10 +14,35 @@ export default function Application(props) {
     interviewers: {}
   });
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const dailyInterviewers = getInterviewersForDay(state, state.day);
+
   const setDay = day => setState({ ...state, day });
   const bookInterview = function(id, interview) {
-    console.log(id, interview);
-  }
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    return axios.put(`http://localhost:8001/api/appointments/${id}`, {interview})
+    .then(setState({...state, appointments}))
+    .catch(err => console.log(err.response.data));
+  };
+  const cancelInterview = function(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: {}
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    return axios.put(`http://localhost:8001/api/appointments/${id}`, {interview: {}})
+    .then(setState({...state, appointments}))
+    .catch(err => console.log(err.response.data));
+  };
 
   const appointmentList = dailyAppointments.map(appointment => {
     const interview = getInterview(state, appointment.interview);
@@ -26,7 +51,9 @@ export default function Application(props) {
       id={appointment.id}
       time={appointment.time}
       interview={interview}
+      interviewers={dailyInterviewers}
       bookInterview={bookInterview}
+      cancelInterview={cancelInterview}
     />
   });
 
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,14 +4,39 @@ import Header from './Header';
 import Show from "./Show"
 import Empty from './Empty';
 import Form from './Form';
+import Status from './Status';
 import useVisualMode from "../hooks/useVisualMode";
-
+import Confirm from './Confirm';
 
 const Appointment = function (props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const CONFIRM = "CONFIRM";
+  const SAVING = "SAVING";
+  const DELETING = "DELETING";
   const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
+
+  const save = function(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    transition(SAVING);
+    props.bookInterview(props.id, interview)
+    .then(() => {
+      transition(SHOW);
+    })
+  };
+
+  const onDelete = function() {
+    //transition(CONFIRM);
+    transition(DELETING);
+    props.cancelInterview(props.id)
+    .then(() => {
+      transition(EMPTY);
+    })
+  };
 
   return (
     <article className="appointment">
@@ -21,13 +46,17 @@ const Appointment = function (props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+          onDelete={onDelete}
         />
       )}
       {mode === CREATE && <Form
-       interviewers={[]}
-       onSave={()=>console.log("hi")}
+       interviewers={props.interviewers}
+       onSave={save}
        onCancel={back}
        />}
+       {mode === SAVING && <Status message="Saving" />}
+       {mode === DELETING && <Status message="Deleting" />}
+       {mode === CONFIRM && <Confirm />}
     </article>
   );
 }

89e8b7742893c2506854498c31faeff3d41c5b8a
Bianca Fu <0509biancafu@gmail.com>
2023-01-18 15:10:51 -0700

    wrote test for loading data in application using async

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,26 +2,31 @@ import React from "react";
 import classNames from "classnames";
 import "components/DayListItem.scss";
 
-const formatSpots = function(spots) {
+const formatSpots = function (spots) {
   if (spots === 0) {
     return "no spots remaining";
   } else if (spots === 1) {
     return "1 spot remaining";
-  } 
+  }
   return `${spots} spots remaining`;
 }
 
 export default function DayListItem(props) {
   const full = props.spots === 0 ? true : false;
   const dayClass = classNames('day-list__item',
-   {'day-list__item--selected':props.selected,
-    'day-list__item--full':full
-  });
-  
+    {
+      'day-list__item--selected': props.selected,
+      'day-list__item--full': full
+    });
+
 
   return (
-    <li className={dayClass} onClick={props.setDay}>
-      <h2 className="text--regular">{props.name}</h2> 
+    <li
+      className={dayClass}
+      onClick={props.setDay}
+      data-testid="day"
+    >
+      <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
